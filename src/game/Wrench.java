a0e75e952eec1b187382c1ca8a17dc378640b326
Justin_Chuah <jchu0056@student.monash.edu>
2022-04-26 22:56:00 +0800

    Addded price to items

--- src/game/Wrench.java
+++ src/game/Wrench.java
@@ -3,8 +3,14 @@ package game;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 
 public class Wrench extends WeaponItem {
+    private final int price;
     public Wrench(){
         super("Wrench", 'W', 50, "Bonk", 80);
         this.addCapability(Status.HAS_WRENCH);
+        this.price = 200;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
