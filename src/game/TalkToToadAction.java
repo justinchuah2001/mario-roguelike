1c2eebde9e54f892fd8aedc604be01a575ea3296
jia zheng <jcha0170@student.monash.edu>
2022-04-27 11:06:01 +0800

    implements monologue

--- src/game/TalkToToadAction.java
+++ src/game/TalkToToadAction.java
@@ -4,6 +4,8 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 
+import java.util.ArrayList;
+
 public class TalkToToadAction extends Action {
   protected Actor target;
 
@@ -18,6 +20,16 @@ public class TalkToToadAction extends Action {
 
   @Override
   public String menuDescription(Actor actor) {
+    ArrayList<String> monologues = new ArrayList<>();
+    monologues.add("The Princess is depending on you! You are our only hope.");
+    monologues.add("Being imprisoned in these walls can drive a fungus crazy :(");
+    if (!actor.hasCapability(Status.HAS_WRENCH)){
+      monologues.add("You might need a wrench to smash Koopa's hard shells.");
+    }
+    if (actor.hasCapability(Status.TALL)){
+      monologues.add("You better get back to finding the Power Stars.");
+    }
+
     return null;
   }
 }
