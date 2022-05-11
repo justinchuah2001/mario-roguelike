8aeb28f202a2185cd7941669873d39f8762c80cd
ckao0003 <ckao0003@student.monash.edu>
2022-05-11 14:37:29 +0800

    Added fire ground and fire flower class

---
+++ src/game/ground/Fire.java
@@ -0,0 +1,12 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+public class Fire extends Ground {
+    /**
+     * Constructor.
+     */
+    public Fire() {
+        super('v');
+    }
+}
---
+++ src/game/items/FireFlower.java
@@ -0,0 +1,12 @@
+package game.items;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public class FireFlower extends Item {
+    /***
+     * A constructor for the fire flower
+     */
+    public FireFlower() {
+        super("Fire Flower", 'f', true);
+    }
+}
