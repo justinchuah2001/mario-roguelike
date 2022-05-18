a6374dcd9c281e33af27aeb33334c1ef734d740c
jia zheng <jcha0170@student.monash.edu>
2022-05-18 14:23:21 +0800

    reset bowser

--- src/game/actors/Bowser.java
+++ src/game/actors/Bowser.java
@@ -8,9 +8,11 @@ import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.Monologue;
+import game.Status;
 import game.items.Key;
+import game.reset.Resettable;
 
-public class Bowser extends Enemy {
+public class Bowser extends Enemy implements Resettable {
   private final static String[] sentences = {"What was that sound? Oh, just a fire.",
           "Princess Peach! You are formally invited... to the creation of my new kingdom!",
           "Never gonna let you down!",
@@ -23,9 +25,10 @@ public class Bowser extends Enemy {
    */
   public Bowser() {
     super("Bowser", 'B', 500);
-    behaviours.remove(10);
-    this.addItemToInventory(new Key());
     this.monologue = new Monologue(this, sentences);
+    this.behaviours.remove(10);
+    this.addItemToInventory(new Key());
+    this.registerInstance();
   }
 
   @Override
@@ -35,7 +38,15 @@ public class Bowser extends Enemy {
 
   @Override
   public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+    if(this.hasCapability(Status.RESET)){
+      this.increaseMaxHp(0);
+    }
     this.monologue.show(display);
     return new DoNothingAction();
+  }
+
+  @Override
+  public void resetInstance() {
+    this.addCapability(Status.RESET);
   }
 }
